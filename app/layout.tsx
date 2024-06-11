85856891fbe7b39dbb0d9bec7d2c779678632e47
mjr2595 <michaeljohnraymond@gmail.com>
2024-06-10 23:42:20 -0500

    chore: Update layout to pass font prop to Header and Footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        <Header font="inter" />
         {children}
-        <Footer />
+        <Footer font="inter" />
       </body>
     </html>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,13 @@
-const Footer = () => {
-  return <div>Footer</div>;
+const Footer = ({ font }: { font: string }) => {
+  return (
+    <footer className="py-12 bg-gray-952 text-gray-953">
+      <div
+        className={`max-w-[100rem] px-12 mx-auto flex justify-between ${font}`}
+      >
+        <p className="text-xl">© Michaelsoft Inc.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

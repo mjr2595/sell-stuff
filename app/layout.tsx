e75ce599c783538ea6aff8d2244160177b3ae74c
mjr2595 <michaeljohnraymond@gmail.com>
2024-06-29 14:20:45 -0500

    chore: Update layout to use Nunito font and add background color to main content area

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito, Josefin_Sans } from "next/font/google";
 import "./globals.css";
+import { getCanonicalUrl } from "@/utils";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Nunito({ subsets: ["latin"] });
+const bodyFont = Josefin_Sans({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Sell Stuff",
-  description: "Sell yer stuff",
+  description: "go on n sell yo stuff",
+  alternates: {
+    canonical: getCanonicalUrl(),
+  },
 };
 
 export default function RootLayout({
@@ -19,9 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <div className="bg-gray-951 py-10">{children}</div>
-        <Footer />
+        <Header font={bodyFont.className} />
+        <hr className="h-0.5 bg-gray-100 border-0"></hr>
+        <div className="bg-gray-951 py-12">{children}</div>
+        <Footer font={bodyFont.className} />
       </body>
     </html>
   );
